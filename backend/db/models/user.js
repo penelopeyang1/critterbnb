79f1aed25b238503f0422eadd525183506eee691
Penelope Yang <penelope.yang21@gmail.com>
2024-05-15 03:24:00 -0500

    Completed Back-End

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -36,7 +36,15 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
-      }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
     },
     {
       sequelize,
