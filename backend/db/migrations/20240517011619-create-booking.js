13a48960feb439d93dc9011968ef0fda2361002e
Penelope Yang <penelope.yang21@gmail.com>
2024-05-18 16:35:31 -0500

    fixed migrations

--- backend/db/migrations/20240517011619-create-booking.js
+++ backend/db/migrations/20240517011619-create-booking.js
@@ -1,4 +1,8 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -41,9 +45,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Bookings');
+    return queryInterface.dropTable(options);
   }
 };
